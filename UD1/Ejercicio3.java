b2fe210dcd13ed59d971edb61344812f52358a92
ANDR3LU <apazodomi@ieschandomonte.edu.es>
2025-10-01 14:22:22 +0200

    a

---
+++ UD1/Ejercicio3.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+public class Ejercicio3 {
+    Random rnd = new Random();
+    int[] partidos;
+    
+
+    public static void main(String[] args) {
+        Int
+    }
+}
